5651bff026254283d05558c1bfd7addfe5409a6c
AlexandraMomoka <tishenkoaleksandra2002@gmail.com>
2022-01-20 22:48:24 +0200

    Second commit

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $('.www').next().css('color', 'red');
 
 $('h2 i').css('color', 'red');
 
-// $('h2 > i').css('color', 'red');
+$('h2 > i').css('color', 'red');
 
 $('h2:not(.test)').css('color', 'red');
 
